746a864c599d881de5d7b4e68771e2c4ae2fa12c
Tom Parry <tomparry@Brett.Dangerfield>
2020-01-21 10:26:24 -0700

    extra comments

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from flask.cli import FlaskGroup
 from project import app
 
+# create FlaskGroup instance to extend normal CLI with commands related to Flask app
 cli = FlaskGroup(app)
 
 if __name__ == '__main__':
--- project/config.py
+++ project/config.py
@@ -1,11 +1,16 @@
+
+# base class
 class BaseConfig:
     TESTING = False
 
+# class for development, testing off
 class DevelopmentConfig(BaseConfig):
     pass
 
+# class for testing, testing on
 class TestingConfig(BaseConfig):
     TESTING = True
 
+# class to use in production, testing off
 class ProductionConfig(BaseConfig):
     pass
